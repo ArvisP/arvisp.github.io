8f1c1f0228c0c8b4e79f0ac6bb3a9183102fdc74
ArvisP <arvis.peralta@gmail.com>
2020-01-21 17:00:28 -0500

    Updates

---
+++ precache-manifest.4bbf835c4d2035f69c49ae16eaf560d6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f688d1e63b9e568a9d7b0e30d1fd00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5483fb8864949512d78",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "25c8578fe9b94b5c67d0",
+    "url": "/static/css/main.de0c838c.chunk.css"
+  },
+  {
+    "revision": "a5483fb8864949512d78",
+    "url": "/static/js/2.5b2f1f61.chunk.js"
+  },
+  {
+    "revision": "a1a92127ad33a115b7d7fbd6c506e45d",
+    "url": "/static/js/2.5b2f1f61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25c8578fe9b94b5c67d0",
+    "url": "/static/js/main.541dffc3.chunk.js"
+  },
+  {
+    "revision": "4e81e6700ec56eb4ae23",
+    "url": "/static/js/runtime-main.5eb8e6b3.js"
+  },
+  {
+    "revision": "fd10f22eebd252aad2cdfa6f76af6738",
+    "url": "/static/media/A1.fd10f22e.png"
+  },
+  {
+    "revision": "a6ec7b60fd9ea4394b264e94afacb0b4",
+    "url": "/static/media/NYC.a6ec7b60.jpg"
+  },
+  {
+    "revision": "611a3f2c70ec9ad3e6f6636380be6183",
+    "url": "/static/media/apache-icon.611a3f2c.png"
+  },
+  {
+    "revision": "3de0e327022ad5af22b7add5fe342cf0",
+    "url": "/static/media/aws-icon.3de0e327.png"
+  },
+  {
+    "revision": "07594703f4dc4c780ac9c68da75af382",
+    "url": "/static/media/cpp-icon.07594703.png"
+  },
+  {
+    "revision": "b62700357c75399198c20794ba0936ba",
+    "url": "/static/media/dart-icon.b6270035.png"
+  },
+  {
+    "revision": "7e482d7f09beab709b9b882ded9891a6",
+    "url": "/static/media/flask-icon.7e482d7f.png"
+  },
+  {
+    "revision": "a90713d6e90d6ddcf5801672e9661f6a",
+    "url": "/static/media/html-icon.a90713d6.png"
+  },
+  {
+    "revision": "46659831145900027eaae3c8ce7234d5",
+    "url": "/static/media/java-icon.46659831.jpg"
+  },
+  {
+    "revision": "845969b398d33389a01ee0303e056df0",
+    "url": "/static/media/js-icon.845969b3.png"
+  },
+  {
+    "revision": "09ed2051ed0b6e784bede6e70c64e9e7",
+    "url": "/static/media/mongodb-icon.09ed2051.png"
+  },
+  {
+    "revision": "34f0a83f7ca8c4ab1bccce4246d62d25",
+    "url": "/static/media/nodejs-icon.34f0a83f.png"
+  },
+  {
+    "revision": "19c658556b93c6efb06a7c431bd9ed80",
+    "url": "/static/media/paperTexture.19c65855.jpg"
+  },
+  {
+    "revision": "86bce2e27b68e3ea5206145eb09b2674",
+    "url": "/static/media/python-icon.86bce2e2.ico"
+  },
+  {
+    "revision": "f88890ddd5addd242eadbbb86f2d6195",
+    "url": "/static/media/react-icon.f88890dd.png"
+  },
+  {
+    "revision": "e30ffe88468133b594a39ad56bea99bf",
+    "url": "/static/media/sql-icon.e30ffe88.png"
+  }
+]);
--- src/Info/WhatICanDo.js
+++
@@ -1,3 +0,0 @@
-const WhatICanDo = "I'm passionate about creating solutions to simplify people's lives. Creating these solutions involves determining requirements, determining what success looks like, analyzing different approaches, all while developing unit tests and documentation. As a software developer, I try to be a jack of all trades who's always loyal to Agile and Waterfall."
-
-export default WhatICanDo
